76374b54a8204318d6739733d3de2b7811c8f108
andrew mendela <jurgeon018@gmail.com>
2020-10-31 20:59:12 +0200

    s

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('api/', include('box.apps.sw_delivery.sw_delivery_auto.api.urls')),  
+    path('api/', include('sw_delivery_auto.api.urls')),  
     path('da/regions_list/', regions_list, name='regions_list'),  
     path('da/areas_list/', areas_list, name='areas_list'),  
     path('da/warehouses_list/', warehouses_list, name='warehouses_list'),  
